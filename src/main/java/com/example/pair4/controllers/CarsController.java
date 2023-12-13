67d7daa6d6f3829a2ec4d4ab1e3158556dee8112
Emre <emreyldrm80@gmail.com>
2023-12-13 23:01:02 +0300

    car controllers updated

--- src/main/java/com/example/pair4/controllers/CarsController.java
+++ src/main/java/com/example/pair4/controllers/CarsController.java
@@ -3,6 +3,7 @@ package com.example.pair4.controllers;
 import com.example.pair4.services.abstracts.CarService;
 import com.example.pair4.services.dtos.car.requests.AddCarRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,8 @@ public class CarsController {
 
 
     private final CarService carService;
+
+    @PostMapping
     private void add(@RequestBody AddCarRequest addCarRequest){
 
         carService.add(addCarRequest);
