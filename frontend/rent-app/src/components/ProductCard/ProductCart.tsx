2d9cdd2b160b5066dd54adbf012ef431497b81bf
Emre <emreyldrm80@gmail.com>
2024-01-14 17:35:56 +0300

    logo fixed

--- frontend/rent-app/src/components/ProductCard/ProductCart.tsx
+++ frontend/rent-app/src/components/ProductCard/ProductCart.tsx
@@ -12,13 +12,13 @@ const ProductCart = (props: Props) => {
     console.log(props.product.image)
     return (
         <div className=''  style={{"backgroundColor":"#DDDCD8"}}>
-            <div className="card m-3 border border-2 border-dark p-2 text-dark bg-opacity-75" style={{"backgroundColor":"FF7F98"}} >
-                <img src={require(`../../assets/images/${props.product.image}`)} className="card-img-top" style={{"height": "220px", "backgroundColor":"#DDDCD8"}} alt="..."/>
+            <div className="card m-3 border border-2 border-dark p-2 text-dark bg-opacity-75 rounded-5" style={{"backgroundColor":"FF7F98"}} >
+                <img src={require(`../../assets/images/${props.product.image}`)} className="card-img-top rounded-5" style={{"height": "220px", "backgroundColor":"#DDDCD8"}} alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{props.product.modelName}</h5>
                         <div className="card-text d-flex align-items-center" style={{"marginBottom": "16px"}}>
                             <div className=''>{props.product.brandName}</div>
-                            <div className='ms-auto'>
+                            <div className='d-md-flex justify-content-md-end'>
                                 <img src={require(`../../assets/logos/${props.product.logo}`)}style={{height:"15%", width:"15%"}} />
                             </div>
                         </div>
@@ -27,7 +27,7 @@ const ProductCart = (props: Props) => {
                         <p className="card-text">{props.product.kilometer}</p>
                         <p className="card-text">{props.product.plate}</p>
                         <div className='d-md-flex justify-content-md-end'>
-                        <a href="#" className="btn btn-outline-secondary">Araç Detayı</a>
+                        <a href="#" className="btn btn-outline-secondary rounded-5">Araç Detayı</a>
                         </div>
                     </div>
             </div>
