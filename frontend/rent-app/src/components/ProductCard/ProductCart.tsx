11c72e7bc30c070cd8f6b5c251eaae6342d660ed
Emre <emreyldrm80@gmail.com>
2024-01-16 01:26:41 +0300

    image path changed

--- frontend/rent-app/src/components/ProductCard/ProductCart.tsx
+++ frontend/rent-app/src/components/ProductCard/ProductCart.tsx
@@ -6,20 +6,18 @@ type Props = {
 }
 
 const ProductCart = (props: Props) => {
-    // const IMG_URL = "../../assets/images/"
-    // const LOGO_URL = "../../assets/logos/"   its not working
 
     console.log(props.product.image)
     return (
         <div className=''  style={{"backgroundColor":"#DDDCD8"}}>
             <div className="card m-3 border border-2 border-dark p-2 text-dark bg-opacity-75 rounded-5" style={{"backgroundColor":"FF7F98"}} >
-                <img src={require(`../../assets/images/${props.product.image}`)} className="card-img-top rounded-5" style={{"height": "220px", "backgroundColor":"#DDDCD8"}} alt="..."/>
+                <img src={props.product.image} className="card-img-top rounded-5" style={{"height": "220px", "backgroundColor":"#DDDCD8"}} alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{props.product.modelName}</h5>
                         <div className="card-text d-flex align-items-center" style={{"marginBottom": "16px"}}>
                             <div className=''>{props.product.brandName}</div>
                             <div className='d-md-flex justify-content-md-end'>
-                                <img src={require(`../../assets/logos/${props.product.logo}`)}style={{ width:"15%", aspectRatio:"3/2", objectFit:"contain"}} />
+                                <img src={props.product.logo}style={{ width:"15%", aspectRatio:"3/2", objectFit:"contain"}} />
                             </div>
                         </div>
                         <p className="card-text">{props.product.dailyPrice}</p>
