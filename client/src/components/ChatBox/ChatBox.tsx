1ef3c49f9b06700548ff6a60a6af5d7adbb51268
Rıdvan Ekinci <101838084+rdvneknc@users.noreply.github.com>
2024-02-20 01:08:26 +0300

    Chatbox z-index adjusted

--- client/src/components/ChatBox/ChatBox.tsx
+++ client/src/components/ChatBox/ChatBox.tsx
@@ -7,7 +7,6 @@ interface ChatboxProps {
 }
 
 const Chatbox: React.FC<ChatboxProps> = ({ isOpen, onClose, openChatbox }) => {
-  // Mesajlar artık bir string yerine { sender: string, text: string } şeklinde bir nesne tutacak
   const [messages, setMessages] = useState<{ sender: string; text: string }[]>([]);
   const [message, setMessage] = useState("");
   const [isButtonVisible, setIsButtonVisible] = useState(true);
@@ -18,7 +17,6 @@ const Chatbox: React.FC<ChatboxProps> = ({ isOpen, onClose, openChatbox }) => {
 
   const handleSendMessage = () => {
     if (message.trim() !== "") {
-      // Kullanıcı tarafından gönderilen mesajlar için sender 'user' olarak ayarlanır
       setMessages([...messages, { sender: 'user', text: message }]);
       setMessage("");
     }
@@ -26,17 +24,16 @@ const Chatbox: React.FC<ChatboxProps> = ({ isOpen, onClose, openChatbox }) => {
 
   const handleChatboxOpen = () => {
     openChatbox();
-    setIsButtonVisible(false); // Düğmeyi gizle
+    setIsButtonVisible(false);
   };
 
   const handleChatboxClose = () => {
     onClose();
-    setIsButtonVisible(true); // Düğmeyi göster
+    setIsButtonVisible(true);
   };
 
   React.useEffect(() => {
     if (isOpen && messages.length === 0) {
-      // Chatbox açıldığında, Max tarafından bir hoşgeldin mesajı ekleyin
       const welcomeMessage = { sender: 'max', text: "Hi there! How can I assist you today?" };
       setMessages([welcomeMessage]);
     }
@@ -45,12 +42,11 @@ const Chatbox: React.FC<ChatboxProps> = ({ isOpen, onClose, openChatbox }) => {
   return (
     <>
       {isOpen && (
-        <div className="fixed bottom-6 right-6 bg-white rounded-lg p-8 border border-purple-600 max-w-md w-full shadow-lg">
+        <div className="fixed bottom-6 right-6 bg-white rounded-lg p-8 border border-purple-600 max-w-md w-full shadow-lg" style={{ zIndex: 9999 }}> {/* zIndex eklenen satır */}
           <h2 className="text-2xl font-bold mb-4 text-purple-600 border-b-2 border-purple-600 pb-2">Live Support 🌟</h2>
           <div className="overflow-y-auto max-h-72 mb-4">
             {messages.map((msg, index) => (
               <div key={index} className="text-gray-600 mb-2">
-                {/* Mesajları gönderenine göre etiketle */}
                 <span className="text-purple-600 font-semibold mr-2">{msg.sender === 'user' ? 'You:' : 'Max:'}</span>
                 {msg.text}
               </div>
@@ -60,12 +56,10 @@ const Chatbox: React.FC<ChatboxProps> = ({ isOpen, onClose, openChatbox }) => {
             <input type="text" value={message} onChange={handleMessageChange} className="flex-1 border border-gray-300 rounded-l-lg p-2 focus:outline-none" placeholder="Type your message..." />
             <button onClick={handleSendMessage} className="bg-purple-600 hover:bg-purple-700 text-white py-2 px-4 rounded-r-lg">Send</button>
           </div>
-          {/* Kapatma butonu */}
           <button onClick={handleChatboxClose} className="absolute top-2 right-2 bg-purple-600 hover:bg-purple-700 text-white py-1 px-2 rounded-full text-xs">X</button>
         </div>
       )}
 
-      {/* Düğme göster/gizle */}
       {isButtonVisible && (
         <div className="fixed bottom-6 right-6 p-6 bg-gradient-to-r from-purple-400 to-indigo-500 rounded-full text-white hover:scale-110 transition-transform duration-300 transform cursor-pointer flex items-center justify-center shadow-lg" onClick={handleChatboxOpen} style={{ zIndex: "9999" }}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9" fill="none" viewBox="0 0 24 24" stroke="currentColor">
